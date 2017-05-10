639b8b4096640adaf6dfa98e981e7f512c2a9da7
Nur Amalia Putri <nuramaliaputri97@gmail.com>
2017-05-10 16:49:22 +0700

    tambah halaman login

---
+++ startbootstrap/loginkurir.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>LOGIN</title>
+
+</head>
+
+<body>
+	<form action="tugasPBW2.html" onsubmit="validate()">
+		<h1>LOGIN SEBAGAI KURIR</h1><hr>
+		Email <br>
+		<input type="email" id="email" size="20"><br><br>
+		Password <br>
+		<input type="password" id="password" size="20"><br><br>
+		<input type="Submit" value="Login">
+	</form>
+</body>
+
+</html>
+
+
