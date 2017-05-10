33a55e557de508953a96c482db2bc4247ba29059
Nur Amalia Putri <nuramaliaputri97@gmail.com>
2017-05-10 16:45:10 +0700

    tambah halaman login

---
+++ startbootstrap/loginutama.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>LOGIN</title>
+
+</head>
+
+<body>
+<center>
+	<h1>PILIH LOGIN SEBAGAI</h1>
+	<div>
+	<a href="loginpedagang.php">PEDAGANG</a>
+	</div>
+	<div>
+	<a href="index.php">PEMBELI</a>
+	</div>
+	<div>
+	<a href="loginkurir.php">KURIR</a>
+	</div>
+	</center>
+</body>
+
+</html>
